888c17bc3fe103870780d64999719f368d71b885
100jnghn <gnswhd0415@gmail.com>
2023-11-20 01:07:07 +0900

    플레이어 이동, 카메라 회전 추가

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,8 +7,9 @@ public class GameManager : MonoBehaviour
     public GameObject startSceneUIPanel; // 시작 화면 UI
     public GameObject startSceneCam; // 시작 화면 카메라
     public GameObject playerCam; // 플레이어 시점의 카메라
+    public GameObject player; // 플레이어 객체
 
-    public GameObject cprSituation; // CPR 상황을 관리 (Start Position CPR 오브젝트)
+    public GameObject cprSituation; // CPR 상황을 관리 (Start Position CPR)
 
     public 
 
@@ -27,6 +28,7 @@ public class GameManager : MonoBehaviour
     {
         startSceneUIPanel.SetActive(false); // 시작 화면 UI 끄기
         startSceneCam.SetActive(false); // 시작 화면 카메라 끄기
+        player.SetActive(true); // 플레이어 활성화
         playerCam.SetActive(true); // 플레이어 시점으로 전환
 
     }
@@ -34,6 +36,7 @@ public class GameManager : MonoBehaviour
     // 시작 메뉴 화면에서 심폐소생술 상황 클릭
     public void startCPR()
     {
+        player.transform.position = cprSituation.transform.position; // 시작 위치 설정
         startSituation();
         cprSituation.GetComponent<CPRSituation>().startSituation(); // CPR 상황 시작
     }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,77 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Player : MonoBehaviour
 {
+    public float moveSpeed = 1.3f; // 이동 속도
+    public float jumpPower = 3.5f; // 점프할 때 리지드바디에 가해지는 힘
+    public float camRotateSpeed = 300f; // 마우스 이동에 의한 카메라 회전 속도
+
+    public GameObject camera; // 플레이어 시점의 카메라
+
+    private Rigidbody rigidbody;
+
+    private float xRotate, yRotate, xRotateMove, yRotateMove; // 마우스에 의한 카메라 회전에 관련된 변수들
+
+
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        
+        doMove(); // 플레이어 이동
+        doRotateCamera(); // 마우스로 카메라 회전
+    }
+
+    // 플레이어 이동 함수
+    void doMove()
+    {
+        Vector3 dir = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W)) // W -> 앞으로 이동
+        {
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+            dir += Vector3.forward;
+        }
+        if (Input.GetKey(KeyCode.S)) // S -> 뒤로 이동
+        {
+            transform.Translate(Vector3.back * moveSpeed * Time.deltaTime);
+            dir += Vector3.back;
+        }
+        if (Input.GetKey(KeyCode.A)) // A -> 왼쪽으로 이동
+        {
+            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+            dir += Vector3.left;
+        }
+        if (Input.GetKey(KeyCode.D)) // D -> 오른쪽으로 이동
+        {
+            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+            dir += Vector3.right;
+        }
+        if (Input.GetKeyDown(KeyCode.Space)) // Space -> 점프
+        {
+            rigidbody.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+        }
+
+    }
+
+    // 카메라 회전하는 함수 
+    // 마우스 상하 이동 -> 카메라만 회전 // 마우스 좌우 이동 -> 카메라, 플레이어 회전
+    void doRotateCamera()
+    {
+        xRotateMove = -Input.GetAxis("Mouse Y") * Time.deltaTime * camRotateSpeed; // 마우스 상하 이동
+        yRotateMove = Input.GetAxis("Mouse X") * Time.deltaTime * camRotateSpeed; // 마우스 좌우 이동
+
+        xRotate = camera.transform.eulerAngles.x + xRotateMove; // 상하 회전
+        //xRotate = Mathf.Clamp(xRotate, -90, 90); // 위, 아래 각도를 제한
+
+        yRotate = transform.eulerAngles.y + yRotateMove; // 좌우 회전
+
+        camera.transform.eulerAngles = new Vector3(xRotate, yRotate, 0); // 카메라 회전
+        transform.eulerAngles = new Vector3(0, yRotate, 0); // 플레이어 회전
     }
 }
